527bdb0cb3503863ceaa4c50867a33ee2fc8b598
mlaulin <e_madhu@hotmail.com>
2020-09-07 11:25:12 -0500

    Addressed typos and cleaned code

--- temperatureConversion.py
+++ temperatureConversion.py
@@ -7,24 +7,26 @@ Created on Fri Aug  7 19:44:49 2020
 """
 
 # Get user input
-i = int(input("Enter -1 for converting from Fahrenheit to Celsius and  2 for converting from Celsius to Fahrenheit: "))
+i = int(input("Enter 1 for converting from Fahrenheit to Celsius and  2 for converting from Celsius to Fahrenheit: "))
 
 
 def f2c():
     # Ask for user input in Fahrenheit
     f = float(input("Enter the temperature in Fahrenheit: "))
+
     # Convert to Fahrenheit
     c = (f - 32) * 5 / 9
-    c = float(c)
+
     return c
 
 
 def c2f():
     #  Ask for user input in Celsius
     c = float(input("Enter the temperature in Celsius: "))
+
     # Convert to Fahrenheit
     f = (c * (9 / 5)) + 32
-    f = float(f)
+
     return f
 
 
